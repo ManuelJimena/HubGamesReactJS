563938d6b3084204d00090db39f69e67c78adf44
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-10-17 23:17:24 +0200

    refact game

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,17 +1,107 @@
 import './Game.css';
 
+import React, { useEffect, useState } from 'react';
+
 import Board from '../Board/Board';
-import Button from '../Button/Button';
-import Message from '../Message/Message';
+import Button from '../Button/Button.jsx';
+import Message from '../Message/Message.jsx';
 
 const Game = () => {
+  const [state, setState] = useState({
+    isStarted: false,
+    turn: 'X',
+    board: Array(3)
+      .fill()
+      .map(() => Array(3).fill(null)),
+    winner: null,
+  });
+
+  const validateBoard = (board) => {
+    let rowValues = board.map((row) => row.join(''));
+    if (rowValues.includes('XXX') || rowValues.includes('OOO')) {
+      return board[0][0];
+    }
+
+    let colValues = board
+      .reduce(
+        (acc, cur) => {
+          acc[0].push(cur[0]);
+          acc[1].push(cur[1]);
+          acc[2].push(cur[2]);
+          return acc;
+        },
+        [[], [], []],
+      )
+      .map((col) => col.join(''));
+    if (colValues.includes('XXX') || colValues.includes('OOO')) {
+      return board[0][0];
+    }
+
+    let diagonalValues = [
+      board[0][0] + board[1][1] + board[2][2],
+      board[0][2] + board[1][1] + board[2][0],
+    ];
+    if (diagonalValues.includes('XXX') || diagonalValues.includes('OOO')) {
+      return board[1][1];
+    }
+
+    if (board.flat().every((cell) => cell !== null)) {
+      return 'draw';
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    const winner = validateBoard(state.board);
+    if (winner !== null) {
+      setState((prevState) => ({
+        ...prevState,
+        winner: winner,
+      }));
+    }
+  }, [state.board]);
+
+  const handleStartButtonClick = () => {
+    setState((prevState) => ({
+      ...prevState,
+      isStarted: true,
+    }));
+  };
+
+  const handleCellClick = (row, col) => {
+    const boardCopy = [...state.board];
+    if (boardCopy[row][col] !== null || state.winner !== null) {
+      return;
+    }
+    boardCopy[row][col] = state.turn;
+    setState((prevState) => ({
+      ...prevState,
+      board: boardCopy,
+      turn: prevState.turn === 'X' ? 'O' : 'X',
+    }));
+  };
+
+  const handleResetButtonClick = () => {
+    setState({
+      isStarted: false,
+      turn: 'X',
+      board: Array(3)
+        .fill()
+        .map(() => Array(3).fill(null)),
+      winner: null,
+    });
+  };
+
   return (
     <div className="game">
-      <Message />
-      <div>
-        <Board />
-      </div>
-      <Button />
+      <Message turn={state.turn} winner={state.winner} />
+      <Board board={state.board} handleCellClick={handleCellClick} />
+      <Button
+        isStarted={state.isStarted}
+        handleButtonClick={
+          state.isStarted ? handleResetButtonClick : handleStartButtonClick
+        }
+      />
     </div>
   );
 };
