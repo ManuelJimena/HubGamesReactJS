db8c10e81783bf0a33841427522ed723c7531892
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-10-17 23:38:03 +0200

    refact game

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,8 +1,10 @@
-import "./Game.css";
-import Board from "../Board/Board";
+import './Game.css';
+
+import React, { useEffect, useState } from 'react';
+
+import Board from '../Board/Board';
 import Button from '../Button/Button.jsx';
 import Message from '../Message/Message.jsx';
-import React, { useState, useEffect } from 'react';
 
 const Game = () => {
   const [state, setState] = useState({
@@ -17,28 +19,36 @@ const Game = () => {
   // función para validar el tablero
   const validateBoard = (board) => {
     // validamos filas
-    let rowValues = board.map(row => row.join(""));
-    if (rowValues.includes("XXX") || rowValues.includes("OOO")) {
+    let rowValues = board.map((row) => row.join(''));
+    if (rowValues.includes('XXX') || rowValues.includes('OOO')) {
       return board[0][0];
     }
     // validamos columnas
-    let colValues = board.reduce((acc, cur) => {
-      acc[0].push(cur[0]);
-      acc[1].push(cur[1]);
-      acc[2].push(cur[2]);
-      return acc;
-    }, [[],[],[]]).map(col => col.join(""));
-    if (colValues.includes("XXX") || colValues.includes("OOO")) {
+    let colValues = board
+      .reduce(
+        (acc, cur) => {
+          acc[0].push(cur[0]);
+          acc[1].push(cur[1]);
+          acc[2].push(cur[2]);
+          return acc;
+        },
+        [[], [], []],
+      )
+      .map((col) => col.join(''));
+    if (colValues.includes('XXX') || colValues.includes('OOO')) {
       return board[0][0];
     }
     // validamos diagonales
-    let diagonalValues = [board[0][0] + board[1][1] + board[2][2], board[0][2] + board[1][1] + board[2][0]]
-    if (diagonalValues.includes("XXX") || diagonalValues.includes("OOO")) {
+    let diagonalValues = [
+      board[0][0] + board[1][1] + board[2][2],
+      board[0][2] + board[1][1] + board[2][0],
+    ];
+    if (diagonalValues.includes('XXX') || diagonalValues.includes('OOO')) {
       return board[1][1];
     }
     // Empate
-    if (board.flat().every(cell => cell !== null)) {
-      return "draw";
+    if (board.flat().every((cell) => cell !== null)) {
+      return 'draw';
     }
     // Si no gana nadie
     return null;
@@ -48,12 +58,12 @@ const Game = () => {
   useEffect(() => {
     const winner = validateBoard(state.board);
     if (winner !== null) {
-      setState(prevState => ({
+      setState((prevState) => ({
         ...prevState,
-        winner: winner
-      }))
-    }    
-  }, [state.board])
+        winner: winner,
+      }));
+    }
+  }, [state.board]);
 
   const handleStartButtonClick = () => {
     setState((prevState) => ({
@@ -91,17 +101,14 @@ const Game = () => {
       <Message turn={state.turn} winner={state.winner} />
       {state.isStarted ? (
         <>
-        <Board board={state.board} handleCellClick={handleCellClick} />
-        <Button
-          isStarted={state.isStarted}
-          handleButtonClick={handleResetButtonClick}
-        />
+          <Board board={state.board} handleCellClick={handleCellClick} />
+          <Button
+            isStarted={state.isStarted}
+            handleButtonClick={handleResetButtonClick}
+          />
         </>
       ) : (
-        <Button
-          isStarted={state.isStarted}
-          handleButtonClick={handleStartButtonClick}
-        />
+        <Button isStarted={state.isStarted} handleButtonClick={handleStartButtonClick} />
       )}
     </div>
   );
