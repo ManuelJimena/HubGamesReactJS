e7dc87d7f37e51f4761c7033718f4254c683e9c7
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-10-19 00:08:39 +0200

    refact add page

---
+++ src/pages/Tresenraya/Tresenraya.jsx
@@ -0,0 +1,13 @@
+import Game from "../../components/Game/Game";
+import "./Tresenraya.css";
+
+const Tresenraya = () => {
+
+    return (
+        <>
+        <Game />
+        </>
+    );
+};
+
+export default Tresenraya;
