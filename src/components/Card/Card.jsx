55a40639b87bd45076b11ae47bb737717c4871f3
Admin <Manuel.jimena29@gmail.com>
2023-10-19 08:30:59 +0200

    refact css

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,19 @@
 import './Card.css';
 
-const Card = () => {
-  return <div className="card"></div>;
+const Card = ({ value, isFlipped, onClick }) => {
+  const handleCardClick = () => {
+    if (!isFlipped) {
+      onClick();
+    }
+  };
+
+  return (
+    <div className={`card ${isFlipped ? 'is-flipped' : ''}`} onClick={handleCardClick}>
+      <div className="front"></div>
+      <div className="back"></div>
+      <div className="content">{isFlipped && value}</div>
+    </div>
+  );
 };
 
 export default Card;
--- src/components/CardGame/CardGame.jsx
+++ src/components/CardGame/CardGame.jsx
@@ -1,7 +1,145 @@
 import './CardGame.css';
 
-const CardGame = () => {
-  return <div className="card-game"></div>;
-};
+import React, { useEffect, useState } from 'react';
+
+import Button from '../Button/Button';
+import Card from '../Card/Card';
+
+function CardGame() {
+  const [cards, setCards] = useState([
+    { id: 1, value: '🐶', isFlipped: false },
+    { id: 2, value: '🐶', isFlipped: false },
+    { id: 3, value: '🐭', isFlipped: false },
+    { id: 4, value: '🐭', isFlipped: false },
+    { id: 5, value: '🦊', isFlipped: false },
+    { id: 6, value: '🦊', isFlipped: false },
+    { id: 7, value: '🐨', isFlipped: false },
+    { id: 8, value: '🐨', isFlipped: false },
+    { id: 9, value: '🐯', isFlipped: false },
+    { id: 10, value: '🐯', isFlipped: false },
+    { id: 11, value: '🐮', isFlipped: false },
+    { id: 12, value: '🐮', isFlipped: false },
+    { id: 13, value: '🐸', isFlipped: false },
+    { id: 14, value: '🐸', isFlipped: false },
+    { id: 15, value: '🐢', isFlipped: false },
+    { id: 16, value: '🐢', isFlipped: false },
+    { id: 17, value: '🐙', isFlipped: false },
+    { id: 18, value: '🐙', isFlipped: false },
+    { id: 19, value: '🦀', isFlipped: false },
+    { id: 20, value: '🦀', isFlipped: false },
+  ]);
+
+  const [gameCards, setGameCards] = useState([]);
+  const [isStarted, setIsStarted] = useState(false);
+
+  useEffect(() => {
+    if (gameCards.length === 2) {
+      const [card1, card2] = gameCards;
+      if (card1.value === card2.value) {
+        setCards((prevCards) =>
+          prevCards.map((card) => {
+            if (card.id === card1.id || card.id === card2.id) {
+              return { ...card, isFlipped: true };
+            }
+            return card;
+          }),
+        );
+        setGameCards([]);
+        return;
+      }
+
+      setTimeout(() => {
+        setCards((prevCards) =>
+          prevCards.map((card) => {
+            if (card.id === card1.id || card.id === card2.id) {
+              return { ...card, isFlipped: false };
+            }
+            return card;
+          }),
+        );
+        setGameCards([]);
+      }, 1500);
+    }
+  }, [gameCards]);
+
+  useEffect(() => {
+    if (isStarted) {
+      setCards((prevCards) => {
+        return shuffle([...prevCards]);
+      });
+    }
+  }, [isStarted]);
+
+  const handleCardClick = (card) => {
+    if (!card.isFlipped && gameCards.length < 2) {
+      setGameCards([...gameCards, card]);
+      setCards((prevCards) =>
+        prevCards.map((prevCard) => {
+          if (prevCard.id === card.id) {
+            return { ...prevCard, isFlipped: true };
+          }
+          return prevCard;
+        }),
+      );
+    }
+  };
+
+  const handleButtonClick = () => {
+    if (isStarted) {
+      const resetCards = cards.map((card) => {
+        return { ...card, isFlipped: false };
+      });
+      setCards(resetCards);
+      setGameCards([]);
+      setIsStarted(false);
+    } else {
+      setIsStarted(true);
+    }
+  };
+
+  // Shuffle function
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  return (
+    <div className="card-game">
+      <Button isStarted={isStarted} handleButtonClick={handleButtonClick} />
+      {isStarted ? (
+        <div className="game-board">
+          {cards.map((card) => (
+            <Card
+              key={card.id}
+              value={card.value}
+              image={card.image}
+              isFlipped={card.isFlipped}
+              onClick={() => handleCardClick(card)}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="start-message">
+          <p>Presiona el botón para comenzar</p>
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default CardGame;
