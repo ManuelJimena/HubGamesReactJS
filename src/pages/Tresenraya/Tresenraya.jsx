96fa0bde1df51f22337373b97fde24c69d68aa95
Admin <Manuel.jimena29@gmail.com>
2023-10-19 16:44:57 +0200

    refact tresenraya

--- src/pages/Tresenraya/Tresenraya.jsx
+++ src/pages/Tresenraya/Tresenraya.jsx
@@ -1,10 +1,12 @@
 import './Tresenraya.css';
 
 import Game from '../../components/Game/Game';
+import Title from '../../components/Title/Title';
 
 const Tresenraya = () => {
   return (
     <>
+      <Title text="Tres en raya" />
       <Game />
     </>
   );
