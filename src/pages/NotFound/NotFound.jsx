6799ca6c2810f5c4cd4ce51811e75bf0a7859f37
Admin <Manuel.jimena29@gmail.com>
2023-10-19 16:47:51 +0200

    refact notfound

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -2,17 +2,22 @@ import './NotFound.css';
 
 import { Link } from 'react-router-dom';
 
+import Title from '../../components/Title/Title';
+
 const NotFound = () => {
   return (
-    <div className="NotFound">
-      <img
-        src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1697219458/21467706_q8uxfs.webp"
-        alt="404 not found"
-      ></img>
-      <Link to="/">
-        <button className="btn-notfound">Go to home</button>
-      </Link>
-    </div>
+    <>
+      <Title text="404 Not Found" />
+      <div className="NotFound">
+        <img
+          src="https://res.cloudinary.com/dhjmt9vvq/image/upload/v1697219458/21467706_q8uxfs.webp"
+          alt="404 not found"
+        ></img>
+        <Link to="/">
+          <button className="btn-notfound">Go to home</button>
+        </Link>
+      </div>
+    </>
   );
 };
 
