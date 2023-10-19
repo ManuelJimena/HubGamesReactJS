18f1ce62e148170ca680b0b59b4a021d473ec16b
Admin <Manuel.jimena29@gmail.com>
2023-10-19 08:43:13 +0200

    refact styles

--- src/components/CardGame/CardGame.jsx
+++ src/components/CardGame/CardGame.jsx
@@ -97,19 +97,15 @@ function CardGame() {
     }
   };
 
-  // Shuffle function
   const shuffle = (array) => {
     let currentIndex = array.length,
       temporaryValue,
       randomIndex;
 
-    // While there remain elements to shuffle...
     while (currentIndex !== 0) {
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
 
-      // And swap it with the current element.
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
@@ -127,7 +123,6 @@ function CardGame() {
             <Card
               key={card.id}
               value={card.value}
-              image={card.image}
               isFlipped={card.isFlipped}
               onClick={() => handleCardClick(card)}
             />
